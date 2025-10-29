39d1a4cee492e6566a21a0be93068277ff08631c
aeroperea <aeroperea@gmail.com>
2025-10-29 03:04:56 -0400

    still too much also not divisible

--- user/train_agent.py
+++ user/train_agent.py
@@ -731,7 +731,7 @@ if __name__ == "__main__":
     os.makedirs(EXP_ROOT, exist_ok=True)
 
     # ---- vectorized env build ----
-    n_envs = 50
+    n_envs = 40
 
     # ---- sb3 hyperparams ----
     # note: with vectorized training, total rollout per update = n_steps * n_envs
